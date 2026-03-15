8b4eacb5ccfe5260895b5f25c0e22b4447d132df
krishnakk663 <kurwalekrishna3@gmail.com>
2026-03-15 12:00:24 +0530

    Homepage redirect to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-"use client"
-
-import { useRouter } from "next/navigation"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-
-  const router = useRouter()
-
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#0F172A] text-white">
-
-      <h1 className="text-5xl font-bold mb-6">
-        🌱 Krushi Netra
-      </h1>
-
-      <p className="text-gray-400 text-lg mb-10">
-        AI + IoT Monitoring System
-      </p>
-
-      <button
-        onClick={() => router.push("/login")}
-        className="bg-green-500 px-6 py-3 rounded-lg"
-      >
-        Open Dashboard
-      </button>
-
-    </div>
-  )
+  redirect("/login")
 }
